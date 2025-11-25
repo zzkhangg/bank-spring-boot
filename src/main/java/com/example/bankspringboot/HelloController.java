76f4962cbaa0d29e6c97d13c21f3b0b24b1dd12c
zzkhangghihi <khang.vuong@student.uq.edu.au>
2025-11-25 10:59:09 +0700

    Initial commit.

---
+++ src/main/java/com/example/bankspringboot/HelloController.java
@@ -0,0 +1,12 @@
+package com.example.bankspringboot;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/")
+    public String index() {
+        return "Hello World from Spring Boot!";
+    }
+}
